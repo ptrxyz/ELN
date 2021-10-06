d5c521e299ea2bacfa7e8a7522ceb89983f7bc53
Benjamin Lutz <b.lutz@kit.edu>
2021-10-06 16:28:38 +0200

    Added checks for folders (exist, can write)

--- initScript.sh
+++ initScript.sh
@@ -6,7 +6,22 @@ if ! mount | grep 'on /shared' 2>&1 1>/dev/null; then
     echo The shared folder is not correctly connected as volume. Please make sure that a folder _shared_ is available next to the docker-compose.yml file.
 fi
 
+[[ ! -d "/shared/eln" ]] && echo Folder /shared/eln does not exist. Please create it.
+[[ ! -d "/shared/eln/config" ]] && echo Folder /shared/eln/config does not exist. Please create it.
+[[ ! -d "/shared/eln/log" ]] && echo Folder /shared/eln/log does not exist. Please create it.
+[[ ! -d "/shared/eln/public" ]] && echo Folder /shared/eln/public does not exist. Please create it.
+[[ ! -d "/shared/eln/tmp" ]] && echo Folder /shared/eln/tmp does not exist. Please create it.
+[[ ! -d "/shared/eln/uploads" ]] && echo Folder /shared/eln/uploads does not exist. Please create it.
+
 # check write permissions in folder
+[[ ! -w "/shared/eln" ]] && echo Folder /shared/eln has no write access. Please grant it.
+[[ ! -w "/shared/eln/config" ]] && echo Folder /shared/eln/config has no write access. Please grant it.
+[[ ! -w "/shared/eln/log" ]] && echo Folder /shared/eln/log has no write access. Please grant it.
+[[ ! -w "/shared/eln/public" ]] && echo Folder /shared/eln/public has no write access. Please grant it.
+[[ ! -w "/shared/eln/tmp" ]] && echo Folder /shared/eln/tmp has no write access. Please grant it.
+[[ ! -w "/shared/eln/uploads" ]] && echo Folder /shared/eln/uploads has no write access. Please grant it.
+
+
 # check existance of certain files?
 # copy files - still needed with new Dockerfile?
 
