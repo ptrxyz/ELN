1dba8e178e43f739b24e235ea99f1ff499ef1818
Benjamin Lutz <b.lutz@kit.edu>
2021-10-06 10:12:19 +0200

    added dummy lines from old init script

--- initScript.sh
+++ initScript.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo Running the init script 
+
+# export INITIALIZE=yes
+# waitForDB
+# execute "${INIT_BASE}/init-scripts/library/shared-storage-init.sh"
+# execute "${INIT_BASE}/init-scripts/library/db-init.sh"
+# [[ "$1" == "dev" ]] && execute "${INIT_BASE}/init-scripts/library/patch4dev.sh"
+# execute "${INIT_BASE}/init-scripts/library/block-pubchem.sh"
+# execute "${INIT_BASE}/init-scripts/library/eln-init.sh"
+# execute "${INIT_BASE}/init-scripts/library/unblock-pubchem.sh"
