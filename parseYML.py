5aa6895e11ce3079e296be4ebf5406b82a39657d
Benjamin Lutz <b.lutz@kit.edu>
2021-10-08 16:18:58 +0200

    Added first draft of YML parsing tool

---
+++ parseYML.py
@@ -0,0 +1,19 @@
+import click
+# import yaml
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option("--prefix", default="DB_", help="string prefix for imported strings")
+@click.argument("file", nargs=1)
+def read(prefix, file):
+    """Read entries from a YML file and add the given PREFIX"""
+    click.echo(f"Reading file {file}")
+    click.echo(f"Adding prefix {prefix}")
+
+cli.add_command(read)
+
+if __name__ == '__main__':
+    cli()
