0cca0d7e21eab6dc1244a504cb2cc2c6707c07bc
Benjamin Lutz <b.lutz@kit.edu>
2021-10-11 08:07:56 +0200

    Added required functionality

--- parseYML.py
+++ parseYML.py
@@ -1,4 +1,5 @@
 import click
+from click.utils import echo
 import yaml
 
 @click.group()
@@ -6,15 +7,31 @@ def cli():
     pass
 
 @click.command()
-@click.option("--prefix", default="DB_", help="string prefix for imported strings")
+@click.option("--prefix", default="", help="string prefix for imported strings")
+@click.option("--upper", is_flag=True, default=False, help="returned keywords should be uppercase letters")
 @click.argument("file", nargs=1)
-def read(prefix, file):
+@click.argument("keywords", nargs=1)
+def read(prefix, upper, file, keywords):
     """Read entries from a YML file and add the given PREFIX"""
-    click.echo(f"Reading file {file}")
-    click.echo(f"Adding prefix {prefix}")
     stream = open(file, "r")
-    fileContent = yaml.load(stream)
-    click.echo(fileContent)
+    fileContent = yaml.safe_load(stream)
+    for keyword in keywords.split("."):
+        if fileContent.get(keyword):
+            fileContent = fileContent[keyword]
+
+    if type(fileContent) is dict:
+        for item in fileContent:
+            if type(item) is str:
+                if upper:
+                    click.echo(prefix + item.upper())
+                else:
+                    click.echo(prefix + item)
+
+    else:
+        if upper:
+            click.echo(prefix + fileContent.upper())
+        else:
+            click.echo(prefix + fileContent)
 
 cli.add_command(read)
 
