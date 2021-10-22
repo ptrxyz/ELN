146a091e09ea65f70d4d786bcfe59d9d25da6ef0
Benjamin Lutz <b.lutz@kit.edu>
2021-10-22 14:41:01 +0200

    added CI script and moved scripts to new folder

---
+++ CI.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+date > log.txt
+
+# rm -rf src/
+ 
+# git clone https://github.com/ComPlat/chemotion_ELN src
+cd src
+echo "based on revision:" >> ../log.txt
+git log | head -1 >> ../log.txt
+cd ..
+
+ 
+# cp -r src/config/* defaultLandscape/config/
+# cp -r src/public/* defaultLandscape/public/
+ 
+# ./build.sh all
+
+# docker-compose run --service-ports eln landscape deploy
+
+# docker-compose run --service-ports eln init
+
+startELN="docker-compose run --service-ports eln starteln"
+$startELN &>/dev/null &
+
+sleep 5
+
+curl -L localhost:3000 >> log.txt
