a03b3242da4c4b3059e015d769c8d1ab4ded234c
Benjamin Lutz <b.lutz@kit.edu>
2021-10-22 15:01:39 +0200

    accidentally commited commented lines

--- CI.sh
+++ CI.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 date > log.txt
 
-# rm -rf src/
+rm -rf src/
  
-# git clone https://github.com/ComPlat/chemotion_ELN src
+git clone https://github.com/ComPlat/chemotion_ELN src
 cd src
 echo "based on revision:" >> ../log.txt
 git log | head -1 >> ../log.txt
 cd ..
 
  
-# cp -r src/config/* defaultLandscape/config/
-# cp -r src/public/* defaultLandscape/public/
+cp -r src/config/* defaultLandscape/config/
+cp -r src/public/* defaultLandscape/public/
  
-# ./build.sh all
+./build.sh all
 
-# docker-compose run --service-ports eln landscape deploy
+docker-compose run --service-ports eln landscape deploy
 
-# docker-compose run --service-ports eln init
+docker-compose run --service-ports eln init
 
 startELN="docker-compose run --service-ports eln starteln"
 $startELN &>/dev/null &
