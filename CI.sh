19b2964c05c298883e56f2b9cee001a2e339a5a6
Benjamin Lutz <b.lutz@kit.edu>
2021-11-02 15:34:49 +0100

    moved productive functionality to prepareBuild.sh

--- CI.sh
+++ CI.sh
@@ -4,31 +4,7 @@ logfile=$(date +"%FT%H%M")_log.txt
 echo "Started at:" > $logfile
 date >> $logfile
 
-rm -rf src/
-rm -rf shared/
-for foldername in $(python3 scripts/parseYML.py read --collect configFileStructure.yml folders.item); do
- echo "creating [shared/eln/${foldername}] ..."; \
- mkdir -p shared/eln/${foldername}; \
-done
-touch shared/eln/config/secrets.yml
- 
-git clone https://github.com/ComPlat/chemotion_ELN src
-cd src
-echo "based on revision:" >> ../$logfile
-git log | head -1 >> ../$logfile
-cd ..
-
-for foldername in $(python3 scripts/parseYML.py read --collect configFileStructure.yml folders.item); do
- echo "Exposing [${foldername}] ..."; \
- rm -r src/${foldername}; \
- ln -s /shared/eln/${foldername} src/${foldername}; \
-done
-
-for filename in $(python3 scripts/parseYML.py read --collect configFileStructure.yml files.item); do
- echo "Exposing [${filename}] ..."; \
- rm src/${filename}; \
- ln -s /shared/eln/${filename} src/${filename}; \
-done
+./prepareBuild.sh $logfile
  
 ./build.sh all
 
--- prepareBuild.sh
+++ prepareBuild.sh
@@ -1,47 +1,26 @@
 #!/bin/bash
 
-checkFolderExists(){
-    if [[ ! -d $1 ]]; then
-        echo "    Folder $1 does not exist. Please create it."
-        return 1
-    else
-        echo "    Found folder $1."
-        return 0
-    fi
-}
-
-checkFolderIsWritable(){
-    if [[ ! -w $1 ]]; then
-        echo "    Folder $1 has no write permission. Please grant it."
-        return 1
-    else
-        echo "    Folder $1 has write permission."
-        return 0
-    fi
-}
+logfile=$1
 
 echo "---- Preparation script ----"
-logfile=$(date +"%FT%H%M")_log.txt
-
-date > $logfile
+# TODO some kind of version file output, e.g. in .version -> needs to be parsed with the CLI "info" option
+# TODO tar ball the src folder as chemotion/app and move it to the container via ADD
+# TODO Do we need to move the template folder as well or can it stay outside of the container?
 
 rm -rf src/
-mkdir -p defaultLandscape/log defaultLandscape/tmp defaultLandscape/uploads
-mkdir -p shared/eln/config shared/eln/log shared/eln/public shared/eln/tmp shared/eln/uploads
-
-echo "Checking out Chemotionn ELN Repository to src/"
+rm -rf shared/
+for foldername in $(python3 scripts/parseYML.py read --collect configFileStructure.yml folders.item); do
+ echo "creating [shared/eln/${foldername}] ..."; \
+ mkdir -p shared/eln/${foldername}; \
+done
+mkdir -p shared/eln/config
+touch shared/eln/config/secrets.yml
+ 
 git clone https://github.com/ComPlat/chemotion_ELN src
 cd src
 echo "based on revision:" >> ../$logfile
 git log | head -1 >> ../$logfile
-# TODO some kind of version file output, e.g. in .version -> needs to be parsed with the CLI "info" option
 cd ..
-
-# TODO prepare contents of the config part in src according to a yaml description (files, folder structure)
-# TODO prepare template folder
-# TODO add dead links in src/ folder (to /share/eln/...)
-# TODO tar ball the src folder as chemotion/app and move it to the container via ADD
-# TODO Do we need to move the template folder as well or can it stay outside of the container?
 
 for foldername in $(python3 scripts/parseYML.py read --collect configFileStructure.yml folders.item); do
  echo "Exposing [${foldername}] ..."; \
@@ -54,12 +33,3 @@ for filename in $(python3 scripts/parseYML.py read --collect configFileStructure
  rm src/${filename}; \
  ln -s /shared/eln/${filename} src/${filename}; \
 done
-
-cp -r src/config/* defaultLandscape/config/
-cp -r src/public/* defaultLandscape/public/
- 
-./build.sh all
-
-docker-compose run --service-ports eln landscape deploy
-
-docker-compose run --service-ports eln init
