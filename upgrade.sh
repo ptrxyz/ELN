32e4d1be0a10140cc1597b9ed13055ef500970fa
Benjamin Lutz <b.lutz@kit.edu>
2021-11-12 14:01:49 +0100

    added first draft of an upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sharedTmp=$(date +"%FT%H%M")_shared
+if [ -d shared ]; then
+    mv shared/ $sharedTmp && mkdir shared && mv $sharedTmp shared/eln/
+else
+   echo "Folder shared/ does not exist. Are you in the right folder of an <1.0.3 setup?"
+   exit 1
+fi
+
+./setup.sh
+
+read -e -p "
+Overwrite configurations files with default files ? [yes/N] " YN
+
+[[ $YN == "yes" ]] && docker-compose run eln landscape deploy
+
+read -e -p "
+
+Run init script (migrating database, generating sprites, compiliing assets) ? [yes/N] " YN
+
+[[ $YN == "yes" ]] && docker-compose run eln init
