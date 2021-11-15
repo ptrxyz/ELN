3741aa55547b6b233630059d89ff11717d7c3d63
Benjamin Lutz <b.lutz@kit.edu>
2021-11-15 10:03:56 +0100

    made upgrade call more specific than just init

--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+echo "You are about to upgrade your existing Chemotion ELN setup."
+echo "Please make sure that you put the new version of docker-compose.yml parallel to this upgrade script."
+echo "Be aware that this script might modifiy existing data and make sure that you have created a backup of your existing data."
+read -e -p "
+Do you wish to proceed ? [yes/N] " YN
+[[ $YN != "yes" ]] && exit 1
+
 sharedTmp=$(date +"%FT%H%M")_shared
 if [ -d shared ]; then
     mv shared/ $sharedTmp && mkdir shared && mv $sharedTmp shared/eln/
 else
-   echo "Folder shared/ does not exist. Are you in the right folder of an <1.0.3 setup?"
+   echo "Folder shared/ does not exist. Are you in the right folder of an <1.0.3 Chemotion ELN setup?"
    exit 1
 fi
 
@@ -16,4 +23,4 @@ Overwrite configurations files with default files ? [yes/N] " YN
 
 read -e -p "
 Run init script (migrating database, generating sprites, compiliing assets) ? [yes/N] " YN
-[[ $YN == "yes" ]] && docker-compose run eln init
+[[ $YN == "yes" ]] && docker-compose run eln upgrade
