e3f398338b4457517672e33bef501630360b60fd
Benjamin Lutz <b.lutz@kit.edu>
2021-12-07 11:00:26 +0100

    placed all folder creation definitions in yml file

--- createSetupScript.sh
+++ createSetupScript.sh
@@ -8,5 +8,9 @@ echo "set -e"
 for foldername in $($YMLPARSE read --collect ${BASEDIR}/configFileStructure.yml folders.item); do
     echo "mkdir -p shared/eln/${foldername}";
 done
-echo "mkdir -p shared/eln/config"
-echo "mkdir -p db-data"
+for foldername in $($YMLPARSE read --collect ${BASEDIR}/configFileStructure.yml createOnlyShared.item); do
+    echo "mkdir -p shared/eln/${foldername}";
+done
+for foldername in $($YMLPARSE read --collect ${BASEDIR}/configFileStructure.yml createOnlyProject.item); do
+    echo "mkdir -p ${foldername}";
+done
