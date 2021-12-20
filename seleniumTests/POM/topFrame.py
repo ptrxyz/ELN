0a1d71b79f4ef29976c81539a475ab111c045498
Benjamin Lutz <b.lutz@kit.edu>
2021-12-20 09:54:39 +0100

    compatibility with older selenium version

--- seleniumTests/POM/topFrame.py
+++ seleniumTests/POM/topFrame.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import time
+
 class TopFrame():
 
     def __init__(self, driver):
@@ -11,8 +13,5 @@ class TopFrame():
 
     def click_logout(self):
         elem = self.driver.find_element(By.CLASS_NAME, self.logout_button_classname)
-        try:
-            wait = WebDriverWait(self.driver, 10)
-            wait.until(EC.element_to_be_clickable(elem))
-        finally:
-            elem.click()
+        time.sleep(1)
+        elem.click()
