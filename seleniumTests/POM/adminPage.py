10c5c04d00f1ec30e2d6da5e991034260d8dd2d2
Mehmood Ghaffar <in1767@kit.edu>
2022-01-28 20:42:08 +0100

    Locators.py and userType.py added for restructuring

--- seleniumTests/POM/adminPage.py
+++ seleniumTests/POM/adminPage.py
@@ -1,41 +1,30 @@
 from selenium.webdriver.common.by import By
+from seleniumTests.POM.locators import Locator
 
 class AdminPage():
 
     def __init__(self, driver):
-        self.driver = driver
-
-        self.usermanagement_link_text = "User Management"
-        self.add_user_button_classname = "btn-primary"
-        self.email_textbox_id = "formControlEmail"
-        self.password_textbox_id = "formControlPassword"
-        self.passwordconfirmation_textbox_id = "formControlPasswordConfirmation"
-        self.firstname_textbox_id = "formControlFirstName"
-        self.lastname_textbox_id = "formControlLastName"
-        self.abbreviation_textbox_id = "formControlAbbr"
-        self.create_button_old_xpath = "/html/body/div[2]/div[2]/div/div/div[2]/div/form/div[9]/div/button[1]"
-        self.create_button_xpath = '//*[@id="createUserTabs-pane-singleUser"]/form/div[8]/div/button'
-        self.close_button_classname = "close"
+        self.driver = driver        
 
     def enter_user_data(self, email, password, firstname, lastname, abbreviation):
-        self.driver.find_element(By.ID, self.email_textbox_id).send_keys(email)        
-        self.driver.find_element(By.ID, self.password_textbox_id).send_keys(password)        
-        self.driver.find_element(By.ID, self.passwordconfirmation_textbox_id).send_keys(password)        
-        self.driver.find_element(By.ID, self.firstname_textbox_id).send_keys(firstname)        
-        self.driver.find_element(By.ID, self.lastname_textbox_id).send_keys(lastname)        
-        self.driver.find_element(By.ID, self.abbreviation_textbox_id).send_keys(abbreviation)        
+        self.driver.find_element(By.ID, Locator.email_textbox_id).send_keys(email)        
+        self.driver.find_element(By.ID, Locator.password_textbox_id).send_keys(password)        
+        self.driver.find_element(By.ID, Locator.passwordconfirmation_textbox_id).send_keys(password)        
+        self.driver.find_element(By.ID, Locator.firstname_textbox_id).send_keys(firstname)        
+        self.driver.find_element(By.ID, Locator.lastname_textbox_id).send_keys(lastname)        
+        self.driver.find_element(By.ID, Locator.abbreviation_textbox_id).send_keys(abbreviation)        
 
     def click_user_management_link(self):
-        self.driver.find_element(By.LINK_TEXT,self.usermanagement_link_text).click()        
+        self.driver.find_element(By.LINK_TEXT, Locator.usermanagement_link_text).click()        
 
     def click_add_user_button(self):
-        self.driver.find_element(By.CLASS_NAME,self.add_user_button_classname).click()        
+        self.driver.find_element(By.CLASS_NAME, Locator.add_user_button_classname).click()        
 
     def click_create(self):
-        self.driver.find_element(By.XPATH, self.create_button_xpath).click()        
+        self.driver.find_element(By.XPATH, Locator.create_button_xpath).click()        
 
     def click_create_old(self):
-        self.driver.find_element(By.XPATH, self.create_button_old_xpath).click()        
+        self.driver.find_element(By.XPATH, Locator.create_button_old_xpath).click()        
 
     def click_close(self):
-        self.driver.find_element(By.CLASS_NAME, self.close_button_classname).click()
+        self.driver.find_element(By.CLASS_NAME, Locator.close_button_classname).click()
---
+++ seleniumTests/POM/userType.py
@@ -0,0 +1,7 @@
+class UserType():
+    def __init__(self, email='ADM', password='PleaseChangeYourPassword', firstname=None, lastname=None, abbreviation=None):
+        self.email = email
+        self.password = password
+        self.firstname = firstname
+        self.lastname = lastname
+        self.abbreviation = abbreviation
